1e3ed116ff9d2917bd3bf607ecc7a9536319ab33
cynicalico <93552084+cynicalico@users.noreply.github.com>
2025-06-14 11:58:25 -0700

    gitignore, clang-format, add mizu

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <filesystem>
-#include <print>
+#include <fmt/format.h>
+#include <vector>
 
 using Cell = std::int64_t;
 
@@ -43,41 +44,39 @@ Fungespace::Fungespace(const std::filesystem::path &path) {
 
 Cell Fungespace::get(std::int64_t x, std::int64_t y) const {
     auto coord = make_fixed_coord_(x, y);
-    std::println("{} {}", coord.x, coord.y);
+    fmt::println("{} {}", coord.x, coord.y);
 
     return 0;
 }
 
-void Fungespace::put(std::int64_t x, std::int64_t y) {
-}
+void Fungespace::put(std::int64_t x, std::int64_t y) {}
 
 Fungespace::FixedCoord_ Fungespace::make_fixed_coord_(std::int64_t x, std::int64_t y) const {
     if (x < 0) {
         if (y < 0) {
             return FixedCoord_{
-                .x = static_cast<std::size_t>(-x - 1), .y = static_cast<std::size_t>(-y - 1),
-                .quadrant = const_cast<std::vector<Cell> &>(nx_ny_)
-            };
+                    .x = static_cast<std::size_t>(-x - 1),
+                    .y = static_cast<std::size_t>(-y - 1),
+                    .quadrant = const_cast<std::vector<Cell> &>(nx_ny_)};
         }
         return FixedCoord_{
-            .x = static_cast<std::size_t>(-x - 1), .y = static_cast<std::size_t>(y),
-            .quadrant = const_cast<std::vector<Cell> &>(nx_py_)
-        };
+                .x = static_cast<std::size_t>(-x - 1),
+                .y = static_cast<std::size_t>(y),
+                .quadrant = const_cast<std::vector<Cell> &>(nx_py_)};
     }
     if (y < 0) {
         return FixedCoord_{
-            .x = static_cast<std::size_t>(x), .y = static_cast<std::size_t>(-y - 1),
-            .quadrant = const_cast<std::vector<Cell> &>(px_ny_)
-        };
+                .x = static_cast<std::size_t>(x),
+                .y = static_cast<std::size_t>(-y - 1),
+                .quadrant = const_cast<std::vector<Cell> &>(px_ny_)};
     }
     return FixedCoord_{
-        .x = static_cast<std::size_t>(x), .y = static_cast<std::size_t>(y),
-        .quadrant = const_cast<std::vector<Cell> &>(px_py_)
-    };
+            .x = static_cast<std::size_t>(x),
+            .y = static_cast<std::size_t>(y),
+            .quadrant = const_cast<std::vector<Cell> &>(px_py_)};
 }
 
-void Fungespace::check_resize_(const FixedCoord_ &coord) {
-}
+void Fungespace::check_resize_(const FixedCoord_ &coord) {}
 
 int main() {
     auto F = Fungespace("");
