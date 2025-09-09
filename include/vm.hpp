caf09e0fba243a21dcab14710b00d7671d197844
cynicalico <228817350+cynicalico@users.noreply.github.com>
2025-09-09 16:01:47 -0700

    temp push

---
+++ include/vm.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace amanita {
+class VM {
+public:
+    VM();
+};
+} // namespace amanita
--- main.cpp
+++ main.cpp
@@ -1,5 +1,46 @@
+#include "argparse.hpp"
+
 #include <fmt/format.h>
 
-int main(int, char *[]) {
-    fmt::println("Hello, world!");
+#include <string>
+#include <vector>
+
+int main(int argc, char *argv[]) {
+    argparse::ArgumentParser program("amanita", "1.0.0");
+
+    argparse::ArgumentParser run_command("run");
+    run_command.add_description("Run Funge-98 program");
+    run_command.add_argument("program").help("Funge-98 program to run").required();
+    run_command.add_argument("-I", "--include")
+            .default_value<std::vector<std::string>>({"."})
+            .append()
+            .help("Additional paths to search for `i`");
+    run_command.add_argument("args").help("Arguments passed to program").remaining();
+
+    program.add_subparser(run_command);
+
+    try {
+        program.parse_args(argc, argv);
+    } catch (const std::exception &e) {
+        fmt::println(stderr, "{}", e.what());
+        fmt::println("");
+        fmt::print(stderr, "{}", program.help().str());
+        return 1;
+    }
+
+    if (program.is_subcommand_used("run")) {
+        std::vector args{run_command.get<std::string>("program")};
+        try {
+            args.insert_range(args.end(), run_command.get<std::vector<std::string>>("args"));
+        } catch (std::logic_error &) {
+        }
+
+        std::vector<std::string> include_paths = {"."};
+        try {
+            include_paths.insert_range(include_paths.end(), run_command.get<std::vector<std::string>>("--include"));
+        } catch (std::logic_error &) {
+        }
+    }
+
+    return 0;
 }
